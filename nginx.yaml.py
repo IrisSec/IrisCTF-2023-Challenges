c096141a1ccafe961ab0308eccba480cc3b17192
sera <>
2023-01-04 02:28:08 -0800

    round 2 of autogen conf

--- nginx.yaml.py
+++ nginx.yaml.py
@@ -6,7 +6,7 @@ with open("ports.json", "r") as f:
     ports = json.load(f)
 
 data1 = "\n".join(f"  {port}: \"default/{challenge}:1337\"" for challenge, port in ports.items())
-ports = {k.replace("_", "-") : v for k, v in ports.items()}
+ports = {k.replace("_", "-")[:15] : v for k, v in ports.items()}
 data2 = "\n".join(f"  - appProtocol: tcp\n    name: {challenge}\n    port: {port}\n    protocol: TCP\n    targetPort: {challenge}" for challenge, port in ports.items())
 data3 = "\n".join(f"        - containerPort: {port}\n          name: {challenge}\n          protocol: TCP" for challenge, port in ports.items())
 
